41061bede946d7510123bf84153596ada2a381db
gerardotamo <gerardotamo@ravn.co>
2022-08-04 11:40:20 -0500

    feat: add session control for home screen

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
-
+import { useEffect, useState } from 'react'
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = window.localStorage.getItem("user");
+    console.log(user)
+    if (user === null) {
+      return navigate("/login", { replace: true });
+    }else{
+      setUser(JSON.parse(window.localStorage.getItem("user") || '{}'))
+    }
+    return () => { }
+  }, [])
+
+  const [user, setUser] = useState()
+
+
   return (
     <div>Home</div>
   )
